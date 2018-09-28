0f6688201d00f37fdc76fe566c72332d31fbba13
AlexKov90 <kavaleuski.alex@gmail.com>
2018-09-28 22:39:39 +0300

    perfomance

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    const brackets = [];
+    let openBrackets = '';
+    let closeBrackets = '';
+    
+    bracketsConfig.forEach(bracketConfig => {
+        openBrackets += bracketConfig[0];
+        closeBrackets += bracketConfig[1];
+    });
+    
+    for (let i = 0; i < str.length; i++) {
+        const bracket = str[i];
+        const indexOfInOpenBrackets = openBrackets.indexOf(bracket);
+        const indexOfInCloseBrackets = closeBrackets.indexOf(bracket);
+        
+        if (indexOfInOpenBrackets !== -1) {
+            if (indexOfInCloseBrackets !== -1) {
+                if (brackets.indexOf(bracket) === -1) {
+                    brackets.push(bracket);
+                } else if (brackets.pop() !== bracket) {
+                    return false;
+                }
+            } else {
+                brackets.push(bracket);
+            }
+        } else if (brackets.pop() !== openBrackets.charAt(indexOfInCloseBrackets)) {
+            return false;
+        }
+    }
+    
+    return brackets.length === 0;
 }
