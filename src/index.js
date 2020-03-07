72b1598c2c746532baa0391b0b390ca3505b06d0
AlexKov90 <kavaleuski.alex@gmail.com>
2020-03-07 17:32:35 +0300

    init: implement function

--- src/index.js
+++ src/index.js
@@ -2,17 +2,17 @@ module.exports = function check(str, bracketsConfig) {
     const brackets = [];
     let openBrackets = '';
     let closeBrackets = '';
-    
+
     bracketsConfig.forEach(bracketConfig => {
         openBrackets += bracketConfig[0];
         closeBrackets += bracketConfig[1];
     });
-    
+
     for (let i = 0; i < str.length; i++) {
         const bracket = str[i];
         const indexOfInOpenBrackets = openBrackets.indexOf(bracket);
         const indexOfInCloseBrackets = closeBrackets.indexOf(bracket);
-        
+
         if (indexOfInOpenBrackets !== -1) {
             if (indexOfInCloseBrackets !== -1) {
                 if (brackets.indexOf(bracket) === -1) {
@@ -27,6 +27,6 @@ module.exports = function check(str, bracketsConfig) {
             return false;
         }
     }
-    
+
     return brackets.length === 0;
 }
